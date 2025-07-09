3eed88f856fc83ca308949ce6cbc573c51514a6b
Shiza Najeeb <shizanajeeb370@gmail.com>
2025-07-09 23:21:11 +0500

    Modifying frontend

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
 connectDB().then(() => {
-    
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  });
 });
