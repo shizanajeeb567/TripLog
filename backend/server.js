35613e7de36564450f6dc52f4e4091c3ffaacec9
Shiza Najeeb <shizanajeeb370@gmail.com>
2025-07-09 23:22:50 +0500

    Modifying backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
-const app = require('./app');
-const connectDB = require('./config/db');
+const app=require('./app');
+const connectDB= require('./config/db');
 require('dotenv').config();
+const PORT= process.env.PORT||8000;
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
 
-const PORT = process.env.PORT || 8000;
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-  });
+    });
 });
